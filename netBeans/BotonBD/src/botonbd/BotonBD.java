1381ffe8477742bff893bae5c37da2ca18ba0c22
Andrea Llovera De Sousa <dam_andreallovera@riberadeltajo.es>
2023-11-15 21:18:18 +0100

    comit

---
+++ netBeans/BotonBD/src/botonbd/BotonBD.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package botonbd;
+
+import java.io.Serializable;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Tarde
+ */
+public class BotonBD extends JButton implements Serializable {
+    
+    private boolean cambiante=true;
+
+    public boolean isCambiante() {
+        return cambiante;
+    }
+
+    public void setCambiante(boolean cambiante) {
+        this.cambiante = cambiante;
+    }
+
+    public BotonBD(boolean cambiante) {
+        this.cambiante=cambiante;
+    }
+
+    public BotonBD() {
+        
+        addActionListener(
+        )
+    }
+   
+    
+    
+}
